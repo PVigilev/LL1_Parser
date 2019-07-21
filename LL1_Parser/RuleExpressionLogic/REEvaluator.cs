b7a5dd0dfa13d59f8e2d64538eca2b7b80574159
Paul <p.vigilev@gmail.com>
2019-07-22 00:04:30 +0100

    RuleExpression and an abstract class RuleExpressionEvaluator are implemented. Using visitor-pattern
    RuleExpressions represents all data that is need to evaluate rule-actions (in '{}'-braces).
    RuleExpressionEvaluator use RuleExpression's API for evaluating these expressions. It gives a Evaluate-function as an API that evaluate RuleExpression instance.

---
+++ LL1_Parser/RuleExpressionLogic/REEvaluator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace LL1_Parser
+{
+    /**/
+
+
+    class REEvaluator : RuleExpressionEvaluator
+    {
+        public override object[] ParsingResult { get; set; }
+    
+        public override object Evaluate(Constant c)
+        {
+            return c.Value;
+        }
+
+        public override object Evaluate(Variable v)
+        {
+            if (v.NumberOfVariable >= ParsingResult.Length)
+                throw new IndexOutOfRangeException($"The rule has {ParsingResult.Length} symbols, but the variable ${v.NumberOfVariable} is greater than rules size");
+            return ParsingResult[v.NumberOfVariable];
+        }
+
+        public override object Evaluate(StaticMethodCalling mc)
+        {
+            object[] args;
+            Type[] types;
+            args = EvaluateExpressions(mc.Args, out types);
+            var method= mc.Type.GetMethod(mc.MethodName, types);
+            if (method == null)
+                throw new MethodNotFoundException($"Static method {mc.Type.FullName}.{mc.MethodName} with passed argument types was not found");
+            if (!method.IsStatic)
+                throw new MethodNotFoundException($"Static method {mc.Type.FullName}.{mc.MethodName} with passed argument types is not static");
+            if(!method.IsPublic)
+                throw new MethodNotFoundException($"Static method {mc.Type.FullName}.{mc.MethodName} with passed argument types is not public");
+            return method.Invoke(null, args);
+        }
+
+        public override object Evaluate(InstanceMethodCalling imc)
+        {
+            object context = imc.Context.Evaluate(this);
+            Type contextType = context.GetType();
+            if (!contextType.IsAssignableFrom(imc.Type))
+                throw new ParserTypeErrorException($"Type {contextType.FullName} is not assignable from {imc.Type.FullName}, so the method {imc.MethodName} can not be invoked with context of type {contextType.FullName}");
+            Type[] argTypes;
+            object[] args = EvaluateExpressions(imc.Args, out argTypes);
+            var method = imc.Type.GetMethod(imc.MethodName, argTypes);
+            if (method == null)
+                throw new MethodNotFoundException($"Instance method {imc.Type.FullName}.{imc.MethodName} with passed argument types was not found");
+            if (method.IsStatic)
+                throw new MethodNotFoundException($"Instance method {imc.Type.FullName}.{imc.MethodName} with passed argument types was not found");
+            if (!method.IsPublic)
+                throw new MethodNotFoundException($"Instance method {imc.Type.FullName}.{imc.MethodName} with passed argument types is not public");
+            return method.Invoke(context, args);
+        }
+
+        public override object Evaluate(ConstructorCalling cc)
+        {
+            object[] args;
+            Type[] types;
+            args = EvaluateExpressions(cc.Args, out types);
+            var constructor = cc.Type.GetConstructor(types);
+            if(constructor == null)
+                throw new MethodNotFoundException($"Constructor for type {cc.Type.FullName} with passed types was not found");
+            return constructor.Invoke(args);
+        }
+
+        private object[] EvaluateExpressions(RuleExpression[] expressions, out Type[] types)
+        {
+            object[] res = new object[expressions.Length];
+            types = new Type[res.Length];
+            for (int i = 0; i < res.Length; i++)
+            {
+                res[i] = expressions[i].Evaluate(this);
+                types[i] = res[i].GetType();
+            }
+            return res;
+        }
+
+        
+    }
+
+}
---
+++ LL1_Parser/RuleExpressionLogic/REExpressionFactory.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LL1_Parser
+{
+    /// <summary>
+    /// Factory-method pattern for creating RuleExpressions
+    /// </summary>
+    abstract class RuleExpressionFactory
+    {
+        public abstract RuleExpression Create();
+    }
+    class VariableCreator : RuleExpressionFactory
+    {
+        uint Id;
+        public VariableCreator(uint id) { Id = id; }
+        public override RuleExpression Create()
+        {
+            return new Variable(Id);
+        }
+    }
+    class ConstantCreator<T> : RuleExpressionFactory
+    {
+        T Value;
+        public ConstantCreator(T val) => Value = val;
+        public override RuleExpression Create()
+        {
+            return new Constant(Value);
+        }
+    }
+    class StaticMethodCallingCreator : RuleExpressionFactory
+    {
+        AssembliesAccessWrapper Wrapper;
+        string FullMethodName;
+        RuleExpressionFactory[] Args;
+        public StaticMethodCallingCreator(AssembliesAccessWrapper w, string mname, RuleExpressionFactory[] a)
+        {
+            Wrapper = w;
+            FullMethodName = mname;
+            Args = a;
+        }
+        public override RuleExpression Create()
+        {
+            RuleExpression[] args = new RuleExpression[Args.Length];
+            for(int i = 0; i < args.Length; i++)
+            {
+                args[i] = Args[i].Create();
+            }
+
+            var lastDot = FullMethodName.LastIndexOf('.');
+            var typename = FullMethodName.Substring(0, lastDot);
+            var methodname = FullMethodName.Substring(lastDot + 1);
+            var type = Wrapper.FindType(typename);
+            return new StaticMethodCalling(type, methodname, args);
+        }
+    }
+
+    class ConstructorCallingCreator : RuleExpressionFactory
+    {
+        AssembliesAccessWrapper Wrapper;
+        string FullTypeName;
+        RuleExpressionFactory[] Args;
+        public ConstructorCallingCreator(AssembliesAccessWrapper w, string tname, RuleExpressionFactory[] a)
+        {
+            Wrapper = w;
+            FullTypeName = tname;
+            Args = a;
+        }
+
+        public override RuleExpression Create()
+        {
+            RuleExpression[] args = new RuleExpression[Args.Length];
+            for (int i = 0; i < args.Length; i++)
+            {
+                args[i] = Args[i].Create();
+            }
+            var type = Wrapper.FindType(FullTypeName);
+            return new ConstructorCalling(type, args);
+        }
+    }
+
+    class InstanceMethodCallingCreator : RuleExpressionFactory
+    {
+        RuleExpressionFactory context;
+        AssembliesAccessWrapper Wrapper;
+        string FullMethodName;
+        RuleExpressionFactory[] Args;
+
+        public InstanceMethodCallingCreator(AssembliesAccessWrapper wrapper, string fullMethodName, RuleExpressionFactory[] args, RuleExpressionFactory context)
+        {
+            Wrapper = wrapper;
+            FullMethodName = fullMethodName;
+            Args = args;
+            this.context = context;
+        }
+
+        public override RuleExpression Create()
+        {
+            RuleExpression cntxt = context.Create();
+            RuleExpression[] args = new RuleExpression[Args.Length];
+            for (int i = 0; i < args.Length; i++)
+            {
+                args[i] = Args[i].Create();
+            }
+
+            var lastDot = FullMethodName.LastIndexOf('.');
+            var typename = FullMethodName.Substring(0, lastDot);
+            var methodname = FullMethodName.Substring(lastDot + 1);
+            var type = Wrapper.FindType(typename);
+            return new InstanceMethodCalling(type, methodname, cntxt, args);
+        }
+    }
+}
