6cdba78b0f4e23f653ccd4b7ea245fadcd13d660
Paul <p.vigilev@gmail.com>
2019-07-22 00:04:30 +0100

    RuleExpression and an abstract class RuleExpressionEvaluator are implemented. Using visitor-pattern
    RuleExpressions represents all data that is need to evaluate rule-actions (in '{}'-braces).
    RuleExpressionEvaluator use RuleExpression's API for evaluating these expressions. It gives a Evaluate-function as an API that evaluate RuleExpression instance.

---
+++ LL1_Parser/RuleExpressionLogic/RuleExpressionEvaluator.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LL1_Parser
+{
+    /**/
+    /// <summary>
+    /// class for evaluation of RuleExpression.
+    /// Used Visitor-template. Visitor part
+    /// </summary>
+    abstract class RuleExpressionEvaluator
+    {
+        public abstract object[] ParsingResult { get; set; }
+        public abstract object Evaluate(Constant c);
+        public abstract object Evaluate(Variable v);
+        public abstract object Evaluate(StaticMethodCalling mc);
+        public abstract object Evaluate(InstanceMethodCalling imc);
+        public abstract object Evaluate(ConstructorCalling cc);
+    }
+
+}
