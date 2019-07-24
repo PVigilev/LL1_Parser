cb60c67fb5414d6e25d689cb30e052b0d86aab0c
Paul <p.vigilev@gmail.com>
2019-07-24 22:39:13 +0100

    AbstractParser represents a parser for LL(1).
    AbstractParser declares an abstract method Object Parse(IList<IToken>)

---
+++ LL1_Parser/Parsers/AbstractParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LL1_Parser
+{
+    abstract class AbstractParser<Token> where Token : IToken
+    {
+        protected Grammar grammar;
+        public abstract object Parse(IList<Token> tokens);
+    }
+}
