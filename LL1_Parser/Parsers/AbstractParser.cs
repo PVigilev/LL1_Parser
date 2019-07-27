0ca9118cd622bd86059a88127bac11cb2ce7b255
Paul <p.vigilev@gmail.com>
2019-07-27 18:14:28 +0100

    NotParsedObject is implemented in the AbstractParser class

--- LL1_Parser/Parsers/AbstractParser.cs
+++ LL1_Parser/Parsers/AbstractParser.cs
@@ -7,7 +7,12 @@ namespace LL1_Parser
 
     public abstract class AbstractParser<Token> where Token : IToken
     {
-        protected Grammar grammar;
+        public class NotParsedObject
+        {
+            private NotParsedObject() { }
+            public static readonly NotParsedObject Instance = new NotParsedObject();
+        }
+        protected Grammar Grammar;
         public abstract object Parse(IList<Token> tokens);
     }
 }
